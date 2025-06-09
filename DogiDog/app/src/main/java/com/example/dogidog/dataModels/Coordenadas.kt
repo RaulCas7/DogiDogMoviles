f4deb2e88ad582fe98c91cc803d0013fc90ddaed
RaulCas7 <raul.casasgomez@iesmiguelherrero.com>
2025-06-09 11:13:35 +0200

    Limpieza codigo

--- DogiDog/app/src/main/java/com/example/dogidog/dataModels/Coordenadas.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dogidog.dataModels
-
-data class Coordenadas(
-    val latitud: Double,
-    val longitud: Double
-)
--- DogiDog/app/src/main/java/com/example/dogidog/dataModels/RecorridoRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dogidog.dataModels
-
-data class RecorridoRequest(
-    val mascotaId: Int,
-    val fecha: String,
-    val distancia: Int,
-    val duracion: Int
-)
